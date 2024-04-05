d99f99fbe12daa9e209a023620523b8d9a03ed49
edagaz <zprashant16@gmail.com>
2024-04-05 11:36:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<Introduction>
+
+<A repo for Quiz application using React.js and CSS.
+Consist of 10 questions, each question has 4 option.
+In the end of the quiz, you get a score board.
+>
---
+++ src/Data/QuizData.js
@@ -0,0 +1,57 @@
+export const QuizData = [
+    {
+        question: "What does HTTP stand for?",
+        options:[ "Hypertext Transport Provider", "Hpertext Transfer Provider",
+                     "Hypertext Transport Protocol", "Hypertext Transfer Protocol"],
+        answer: 4
+    },
+    {
+        question: "React is a ",
+        options:["Software", "Library", "Framework", "Server"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "CSS stand for...",
+        options:["Cascading Style Sheets","Cascading Sheets Style","Car Skating Street","None of the above"],
+        answer: 1
+    },
+    {
+        question: "Which of the following is not a type of cyber attack?",
+        options:["phishing","Ransomware","Firefighting"," Denial of Service (DoS)"],
+        answer: 3
+    },
+    {
+        question: "What is the primary function of a web browser?",
+        options: ["To create and edit web pages", "To search for information on the internet", 
+                   " To display web pages and navigate the World Wide Web", "To secure online transactions and payments"],
+        answer:3
+    },
+    {
+        question: "What protocol is used for securely transferring files over a network?",
+        options: ["FTP (File Transfer Protocol)", "SMTP (Simple Mail Transfer Protocol)",
+                    "HTTP (Hypertext Transfer Protocol)", " SSH (Secure Shell)"],
+        answer:4
+    },
+    {
+        question: "What is the term for a software application that replicates itself and spreads to other computers?",
+        options: ["Virus", "Worm","Trojan horse", "Spyware"],
+        answer:2
+    },
+    {
+        question: "Which of the following programming languages is primarily used for statistical analysis and data visualization?",
+        options: ["Java", "Python","R", "Ruby"],
+        answer:3
+    },
+
+
+];
